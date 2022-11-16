a3f556297eaad00ac31d521c80671bf046a460a3
tomomi adachi <tomoni1997@gmai.com>
2022-11-16 17:54:29 +0900

    問い合わせフォーム

---
+++ test.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('th').on({
+        'mouseenter': function() {
+          $(this).css('color', 'blue');
+        },
+        'mouseleave': function() {
+          $(this).css('color', 'black');
+        }
+      })
+    })
+
+$(function() {
+    $('p').css('background-color', 'gold');
+});
